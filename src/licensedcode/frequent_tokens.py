1c12324fcc61c309d51b39ad1b9bcb61a7119d99
Philippe Ombredanne <pombredanne@nexb.com>
2019-04-16 10:37:55 +0200

    Add some product names as frequent tokens
    Also keep one word per lien for simplicity
    
    Signed-off-by: Philippe Ombredanne <pombredanne@nexb.com>

--- src/licensedcode/frequent_tokens.py
+++ src/licensedcode/frequent_tokens.py
@@ -9887,11 +9887,13 @@ Austria
 Azerbaijan
 Belarus
 Belgium
-Bosnia and Herzegovina
+Bosnia
+Herzegovina
 Bulgaria
 Croatia
 Cyprus
-Czech Republic
+Czech 
+Republic
 Denmark
 Estonia
 Finland
@@ -10515,6 +10517,9 @@ Tommi
 Komulainen
 iki
 fi
+bungisoft
+HSQLDB
+RedBase
 '''
 
 
